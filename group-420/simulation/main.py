91a957fa308ba82cd08a49761077c8bda5875e6f
Michael Plainer <plainer@ymail.com>
2021-09-27 16:02:06 +0200

    Update main

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 import utils.config as configurations
+import utils.dataloader
 import utils.dataloader as dataloader
 
 config = configurations.local_config
 
 
 def main():
-    print("Hello World!")
-
     dataset = tf.data.Dataset.list_files(config["path"])\
-        .map(dataloader.load_image)\
-        .batch(config.batch_size)\
+        .map(dataloader.process_path)\
+        .batch(config["batch_size"])\
         .prefetch(2)
+
+    print(list(dataset.as_numpy_iterator()))
 
 
 if __name__ == "__main__":
