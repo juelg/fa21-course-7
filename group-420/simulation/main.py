2572633f1e0b189920f03c637004b38d73cfa7e4
Michael Plainer <plainer@ymail.com>
2021-09-27 15:21:28 +0200

    Add dataloader structure to main

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -1,11 +1,17 @@
 import tensorflow as tf
 import utils.config as configurations
+import utils.dataloader as dataloader
 
 config = configurations.local_config
 
 
 def main():
     print("Hello World!")
+
+    dataset = tf.data.Dataset.list_files(config["path"])\
+        .map(dataloader.load_image)\
+        .batch(config.batch_size)\
+        .prefetch(2)
 
 
 if __name__ == "__main__":
--- group-420/simulation/utils/config.py
+++ group-420/simulation/utils/config.py
@@ -1,7 +1,9 @@
 server_config = {
     "path": "/share/user*/*.jpg",
+    "batch_size": 100
 }
 
 local_config = {
-    "path": r"<add your local path>"
+    "path": r"<add your local path>",
+    "batch_size": 100
 }
