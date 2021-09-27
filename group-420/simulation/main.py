7b2a12a9ae46600aaf6dd227412e9aab911b0ab8
Michael Plainer <plainer@ymail.com>
2021-09-27 15:08:06 +0200

    Add config

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -1,5 +1,12 @@
+import tensorflow as tf
+import utils.config as configurations
+
+config = configurations.local_config
+
+
 def main():
     print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
---
+++ group-420/simulation/utils/config.py
@@ -0,0 +1,7 @@
+server_config = {
+    "path": "/share/user*/*.jpg",
+}
+
+local_config = {
+    "path": r"<add your local path>"
+}
