face2db7e741d51fabf3413cadf0cf70f724e645
Michael Plainer <plainer@ymail.com>
2021-09-27 17:15:57 +0200

    Remove config from git

--- group-420/simulation/utils/config.py
+++
@@ -1,9 +0,0 @@
-server_config = {
-    "path": "/share/user*/*.jpg",
-    "batch_size": 100
-}
-
-local_config = {
-    "path": r"<add your local path>",
-    "batch_size": 100
-}
---
+++ group-420/simulation/utils/config_example.py
@@ -0,0 +1,9 @@
+server_config = {
+    "path": "/share/user*/*.jpg",
+    "batch_size": 100
+}
+
+local_config = {
+    "path": r"C:\Users\plain\Dropbox\university\21SS\Ferianakademie\data\track1\*.jpg",
+    "batch_size": 1
+}
