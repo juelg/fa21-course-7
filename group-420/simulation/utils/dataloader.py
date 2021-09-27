e2359add514cda90a64047e1db4c077c36bcf494
Michael Plainer <plainer@ymail.com>
2021-09-27 16:00:45 +0200

    Add comment for load_image

--- group-420/simulation/utils/dataloader.py
+++ group-420/simulation/utils/dataloader.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import tensorflow as tf
 
+
 def decode_img(img):
     # convert the compressed string to a 3D uint8 tensor
     img = tf.image.decode_jpeg(img, channels=3)
@@ -10,6 +11,7 @@ def decode_img(img):
 
     return img
 
+
 def process_path(file_path):
 
     # load the raw data from the file as a string
@@ -18,8 +20,9 @@ def process_path(file_path):
 
     return img      
 
+
 def load_image(path):
-    filename = Path(path).stem.split('_')
+    filename = Path(path).stem.split('_')  # index_speed_angle
     
     return process_path(path), (filename[1], filename[2])
 
