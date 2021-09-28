28fc053441f49d59af1e62f548073931f09b7c43
Michael Plainer <plainer@ymail.com>
2021-09-28 09:19:19 +0200

    Add driver API

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -8,8 +8,6 @@ config = configurations.local_config
 
 
 def main():
-    # TODO: https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
-    # buffersize
     dataset = tf.data.Dataset.list_files(config["path"], shuffle=True, seed=420)\
         .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
         .batch(config["batch_size"])\
