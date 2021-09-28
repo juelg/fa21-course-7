0b50117bcfee5645850e11748271a328e79d09e3
Michael Plainer <plainer@ymail.com>
2021-09-28 21:14:57 +0200

    Reformat main

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -3,33 +3,23 @@ from utils.config import config
 import utils.dataloader as dataloader
 import utils.model as models
 
+dataset = tf.data.Dataset.list_files(config["path"], shuffle=True, seed=420)\
+    .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
+    .batch(config["batch_size"])\
+    .prefetch(2)\
+    .shuffle(config["batch_size"], reshuffle_each_iteration=True)
 
-def main():
-    dataset = tf.data.Dataset.list_files(config["path"], shuffle=True, seed=420)\
-        .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
-        .batch(config["batch_size"])\
-        .prefetch(2)\
-        .shuffle(3 * config["batch_size"], reshuffle_each_iteration=True)
+val_dataset = tf.data.Dataset.list_files(config["val_path"], shuffle=True, seed=420)\
+    .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
+    .batch(config["batch_size"])\
+    .prefetch(2)\
+    .shuffle(config["batch_size"], reshuffle_each_iteration=True)
 
-    val_dataset = tf.data.Dataset.list_files(config["val_path"], shuffle=True, seed=420)\
-      .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
-      .batch(config["batch_size"])\
-      .prefetch(2)\
-      .shuffle(3 * config["batch_size"], reshuffle_each_iteration=True)
-    
-    #print(list(dataset.as_numpy_iterator()))
-    for img, label in dataset:
-        #print(img.shape)
-        #print(img)
-        break
+model = models.Model420()
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+model.build((config["batch_size"], 160, 320, 3))
 
-    model = models.Model420()
-    model.compile(optimizer="adam", loss="mse", metrics=["mae"])
-    model.build((config["batch_size"], 160, 320, 3))
-    print(model.summary())
-    model.fit(dataset, validation_data = val_dataset, epochs=config["epochs"])
-    model.save("model", save_format="tf")
+print(model.summary())
 
-
-if __name__ == "__main__":
-    main()
+model.fit(dataset, validation_data=val_dataset, epochs=config["epochs"])
+model.save("model", save_format="tf")
--- group-420/simulation/utils/config_example.py
+++ group-420/simulation/utils/config_example.py
@@ -1,5 +1,6 @@
 config = {
     "path": r"<your path here>/*.jpg",
+    "val_path": r"<your path here>/*.jpg",
     "batch_size": 50,
     "epochs": 10
 }
