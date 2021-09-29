43bb68b621ef66107acf9a8d3844766340b82cf4
Bastin Heinlein <bastian.heinlein@live.de>
2021-09-29 15:09:00 +0200

    Update pidSteeringAngle.py

--- control/pidSteeringAngle.py
+++ control/pidSteeringAngle.py
@@ -24,7 +24,7 @@ def steeringAnglePIDStep(phi: float, dt: float, dt_1: float, d_total: float, cp:
     Returns:
         float: New steering angle
     """
-    phi_new = min(max(phi + cp*dt + cd*(dt-dt_1) + ci*d_total, -np.pi/4), np.pi/4)
+    phi_new = min(max(cp*dt + cd*(dt-dt_1) + ci*d_total, -np.pi/4), np.pi/4)
     print(phi_new, phi, cp*dt + cd*(dt-dt_1) + ci*d_total)
     return phi_new
 
