5faff4e96b9c1f6b03d9d2f006e566c1e0436af2
Your Name <you@example.com>
2021-09-29 10:57:58 +0200

    Fancy filtering of angles using awesome and readable functional programming.

--- pt/dataset.py
+++ pt/dataset.py
@@ -7,17 +7,30 @@ import torch
 import random
 
 class AutoDataset(Dataset):
-    def __init__(self, folders, transforms=None, mirror_prob=0.5):
+    def __init__(self, folders, transforms=None, mirror_prob=0.5, zero_angle_dropout=False, large_angle_dropout=False):
         self.folders = folders
         self.transforms = transforms
         self.mirror_prob = mirror_prob
+        self.zero_angle_dropout = zero_angle_dropout
+        self.large_angle_dropout = large_angle_dropout
         # create list of files
         self.f_list = []
         for i in self.folders:
             to_append = list(Path(i).rglob('*.jpg'))
-            self.f_list += list(map(lambda e: e[1], sorted(zip(map(lambda fname: int(str(fname).split("/")[-1].split("_")[0]), to_append), to_append))))
+            to_append = zip(map(lambda fname: list(map(int, str(fname).replace('.jpg', '').split("/")[-1].split("_")), to_append)), to_append)
 
-        self.f_list =list(filter(lambda fname: int(str(fname).split("/")[-1].split("_")[1]) != 0, self.f_list))
+            minimum_idx = min(map(lambda entry: entry[0][0], to_append))
+            maximum_idx = max(map(lambda entry: entry[0][0], to_append))
+
+            if self.zero_angle_dropout:
+                filter_func_zero = lambda entry: entry[0][2] != 0 or to_append[max(entry[0][0]-1, minimum_idx)][2] == 0 or to_append[min(entry[0][0]+1, maximum_idx)][2] == 0
+                to_append = list(filter(filter_func_zero, to_append))
+
+            if self.large_angle_dropout:
+                filter_func_large = lambda entry: abs(entry[0][2]) <= 900
+                to_append = list(filter(filter_func_large, to_append))
+
+            self.f_list += list(map(lambda e: e[1], sorted(to_append)))
 
 
 
