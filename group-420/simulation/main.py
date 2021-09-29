e87261038612f8a9d18c00d4ca202fcc4dc7da8d
Peter Bauer <st148923tud.uni-stuttgart.de>
2021-09-29 14:15:07 +0200

    add tensorboard

--- group-420/simulation/main.py
+++ group-420/simulation/main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from utils.config import config
 import utils.dataloader as dataloader
 import utils.model as models
+import datetime
 
 dataset = tf.data.Dataset.list_files(config["path"], shuffle=True, seed=420)\
     .map(dataloader.process_path, num_parallel_calls=tf.data.AUTOTUNE)\
@@ -23,7 +24,10 @@ model = models.Model420(config["crop"])
 model.compile(optimizer="adam", loss="mse", metrics=["mae"])
 model.build((config["batch_size"], 160, 320, 3))
 
+# summary and logging
 print(model.summary())
+log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-model.fit(dataset, validation_data=val_dataset, epochs=config["epochs"])
+model.fit(dataset, validation_data=val_dataset, epochs=config["epochs"],callbacks=[tensorboard_callback])
 model.save("model", save_format="tf")
