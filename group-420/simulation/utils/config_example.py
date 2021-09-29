cf44810d111318c6dfd6465b6c91bc95a36b19ff
HelenaKschidock <h.kschidock@gmail.com>
2021-09-29 14:04:40 +0200

    Added instructions for retraining saved model

--- group-420/simulation/utils/config_example.py
+++ group-420/simulation/utils/config_example.py
@@ -3,5 +3,6 @@ config = {
     "val_path": r"<your path here>/*.jpg",
     "batch_size": 50,
     "epochs": 10,
-    "crop" : 60
+    "crop" : 60,
+    "saved_model_path": None or r"<your path to model folder here>"
 }
