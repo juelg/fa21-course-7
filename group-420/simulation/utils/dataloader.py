2e5a1c6320c4c7981d3f43603085a90bf055d9f0
Oliver Lemke <lemke.oliver@icloloud.com>
2021-09-29 10:51:12 +0200

    Added random gaussian blur on images in dataloader.py

--- group-420/simulation/utils/dataloader.py
+++ group-420/simulation/utils/dataloader.py
@@ -1,5 +1,9 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
+
+FLIP_PERCENTAGE = 0.5
+BLUR_PERCENTAGE = 0.4
 
 
 def decode_img(img):
@@ -13,6 +17,27 @@ def decode_img(img):
     return img
 
 
+def apply_flip(img, steering_angle):
+    def flip_true_fn(): return tf.image.flip_left_right(img), -steering_angle
+
+    def flip_false_fn(): return img, steering_angle
+
+    val = tf.random.uniform((), dtype=tf.dtypes.float32)
+    return tf.cond(val < FLIP_PERCENTAGE, true_fn=flip_true_fn, false_fn=flip_false_fn)
+
+
+def apply_gaus_blur(img):
+    blur_size = np.random.randint(2, 5)
+    blur_sigma = np.random.rand() * 2
+    val = tf.random.uniform((), dtype=tf.dtypes.float32)
+
+    def gaus_blur_true_fn(): return tfa.image.gaussian_filter2d(img, blur_size, blur_sigma, "REFLECT", 0)
+
+    def gaus_blur_false_fn(): return img
+
+    return tf.cond(val < BLUR_PERCENTAGE, true_fn=gaus_blur_true_fn, false_fn=gaus_blur_false_fn)
+
+
 def process_path(file_path):
     # load the raw data from the file as a string
     img = tf.io.read_file(file_path)
@@ -20,11 +45,8 @@ def process_path(file_path):
 
     steering_angle = tf.strings.to_number(tf.strings.split(tf.strings.split(file_path, '_')[-1], '.')[0]) / 1000.0
 
-    def true_fn(): return tf.image.flip_left_right(img), -steering_angle
+    img, steering_angle = apply_flip(img, steering_angle)
 
-    def false_fn(): return img, steering_angle
-
-    val = tf.random.uniform((), dtype=tf.dtypes.float32)
-    img, steering_angle = tf.cond(val < 0.5, true_fn=true_fn, false_fn=false_fn)
+    img = apply_gaus_blur(img)
 
     return img, steering_angle
